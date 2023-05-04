72bbbd2fc213d9ea31e410ac32059878e0d549b9
violetstar701 <violetstar701@gmail.com>
2023-05-04 20:12:24 +0300

    beautify message

--- app.py
+++ app.py
@@ -58,59 +58,59 @@ def extract_threads():
 
     obj = next(json_iterator)
 
-    for obj in json_iterator:
-        # Loop through each message in the thread and prints its content.
-        # print(len(obj['messages']))
-        for message in obj['messages']:
-            # Extract the raw message payload and decode it from base64.
-            payload = message['payload']
-            # Decode the message content from base64 and display it.
-            flag = False
-            sender = None
-            sendtime = None
-            for header in payload['headers']:
-                if header["name"] == "From":
-                    sender = header["value"]
-                if header["name"] == "Date":
-                    sendtime = header["value"]
-                if header["name"] != "Delivered-To":
-                    continue
-                if header["value"] == "chelsea@tribecap.co" or header["value"] == "veronica@tribecap.co":
-                    flag = True
-            if flag == True:
-                parts = payload
-                partFlag = False
-                while parts['mimeType'] != 'text/plain':
-                    if parts['mimeType'] == 'text/html':
-                        partFlag = True
-                        break
-                    parts = parts['parts'][0]
-                if not partFlag:
-                    data = parts['body']['data']
-                    content = base64.urlsafe_b64decode(data).decode('utf-8')
-                    content = content.replace("> ", "")
-                    print("\n==================================\n")
-                    lines = content.split("\n")
-                    temp_unit = []
+    # for obj in json_iterator:
+    # Loop through each message in the thread and prints its content.
+    # print(len(obj['messages']))
+    for message in obj['messages']:
+        # Extract the raw message payload and decode it from base64.
+        payload = message['payload']
+        # Decode the message content from base64 and display it.
+        flag = False
+        sender = None
+        sendtime = None
+        for header in payload['headers']:
+            if header["name"] == "From":
+                sender = header["value"]
+            if header["name"] == "Date":
+                sendtime = header["value"]
+            if header["name"] != "Delivered-To":
+                continue
+            if header["value"] == "chelsea@tribecap.co" or header["value"] == "veronica@tribecap.co":
+                flag = True
+        if flag == True:
+            parts = payload
+            partFlag = False
+            while parts['mimeType'] != 'text/plain':
+                if parts['mimeType'] == 'text/html':
+                    partFlag = True
+                    break
+                parts = parts['parts'][0]
+            if not partFlag:
+                data = parts['body']['data']
+                content = base64.urlsafe_b64decode(data).decode('utf-8')
+                content = content.replace("> ", "")
+                print("\n==================================\n")
+                lines = content.split("\n")
+                temp_unit = []
 
-                    beautified_list = []
+                beautified_list = []
 
-                    for line in lines:
-                        while line.startswith(">"):
-                            line = line[1:]
-                        if line != "\r":
-                            line = line.replace("\r", "")
-                            temp_unit.append(line)
-                        else:
-                            result = " ".join(temp_unit)
-                            if not "Disclaimer" in result and not "CONFIDENTIALITY" in result:
-                                beautified_list.append(result)
-                            temp_unit.clear()
+                for line in lines:
+                    while line.startswith(">"):
+                        line = line[1:]
+                    if line != "\r":
+                        line = line.replace("\r", "")
+                        temp_unit.append(line)
+                    else:
+                        result = " ".join(temp_unit)
+                        if not "Disclaimer" in result and not "CONFIDENTIALITY" in result:
+                            beautified_list.append(result)
+                        temp_unit.clear()
 
-                    beautified_message = "\n".join(beautified_list)
+                beautified_message = "\n".join(beautified_list)
 
-                    # print(len(beautified_message))
-                    print(sort_messages(beautified_message, sender, sendtime))
+                # print(len(beautified_message))
+                print(sort_messages(beautified_message, sender, sendtime))
 
 
 if __name__ == "__main__":
